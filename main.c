222f6372e4e7db4dea817287ea6c64354c4e9d47
virzimayhandsyahputra <virzi@mhs.usk.ac.id>
2025-11-22 22:46:32 +0700

    menambahkan main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+typedef Struct {
+    char abjad;
+    int jumlahKataAbjad;
+    int panjangKata;
+    char kata[1024];
+    int frekuensi;
+} InilahNantiYangBakalDitampilinKeOutputnyaKING
+
+int main(){
+    
+    return EXIT_SUCCESS;
+}
